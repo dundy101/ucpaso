8e48f971c3dd5ec82b257bbbb110591eae7b4dd7
dundy101 <dundyhong23@gmail.com>
2026-06-17 22:49:21 -0400

    fix config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next'
 const config: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/ucpaso',
-  assetPrefix: '/ucpaso/',
   images: {
-    unoptimized: true,
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
 }
 export default config
