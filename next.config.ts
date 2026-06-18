62b4a4777fd549f41b9776779a6399387b601742
dundy101 <dundyhong23@gmail.com>
2026-06-17 22:30:44 -0400

    fix routing for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 const config: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/ucpaso',
+  assetPrefix: '/ucpaso/',
   images: {
     unoptimized: true,
   },
